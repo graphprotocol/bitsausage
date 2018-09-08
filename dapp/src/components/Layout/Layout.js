ce8cc56b23ced4ef09fb713045b13c7b97a7ed08
Nena <nena@thegraph.com>
2018-09-08 23:47:46 +0200

    homepage created

--- dapp/src/components/Layout/Layout.js
+++ dapp/src/components/Layout/Layout.js
@@ -1,46 +1,132 @@
 import React from 'react'
 import { Grid, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
+import classnames from 'classnames'
 
-import Button from '../Button/Button'
-import Social from '../Social/Social'
 import styles from './Layout.styles'
+import Button from '../Button/Button'
+import RowItem from '../RowItem/RowItem'
 
-const Layout = ({ name, classes }) => (
-  <Grid className={classes.root}>
-    <span className={classes.shape1} />
-    <span className={classes.shape2} />
-    <span className={classes.shape3} />
-    <span className={classes.shape4} />
-    <Grid container direction="column" className={classes.section}>
-      <Typography className={classes.title}>Welcome {name}!</Typography>
-      <span className={classes.divider} />
-      <Typography className={classes.welcome}>
-        This is a starter app built by our team at The Graph. It uses React,
-        Apollo and Material UI. We also have an example GraphQL endpoint for
-        building your frontend.
-        <br />
-        <br />
-        Check out The Graph Bounty description to start building your cool
-        project. Happy Hacking!
-      </Typography>
-      <Grid container justify="center" className={classes.buttons}>
-        <Button
-          href="http://thegraph.com"
-          target="_blank"
-          text="THEGRAPH.COM"
-        />
-        <Button
-          href="https://github.com/ethberlin-hackathon/ETHBerlin-Bounties/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+%22the+graph+prize%22"
-          target="_blank"
-          text="THE GRAPH BOUNTY"
-          variant="dark"
-        />
+class Layout extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { bid: 0 }
+
+    this.handleOpenClick = this.handleOpenClick.bind(this)
+    this.handleCloseClick = this.handleCloseClick.bind(this)
+    this.onBidChange = this.onBidChange.bind(this)
+  }
+
+  handleOpenClick() {
+    if (this.state.bid === 13) {
+      document.querySelector('.overlay').style.display = 'block'
+      window.scroll({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  handleCloseClick() {
+    document.querySelector('.overlay').style.display = 'none'
+    const bid = document.querySelector('#bid').offsetTop
+    console.log('bid: ', bid)
+    window.scroll({
+      top: bid,
+      behavior: 'smooth'
+    })
+  }
+
+  onBidChange(e) {
+    this.setState({ bid: parseInt(e.target.value) })
+  }
+
+  render() {
+    const { classes } = this.props
+
+    return (
+      <Grid className={classes.root}>
+        <div className={classnames(classes.overlay, 'overlay')}>
+          <Grid className={classes.congrats}>
+            <Grid>congrats, The</Grid>
+            <Grid className={classes.big}>Bratwurst</Grid>
+            <Grid>is yours</Grid>
+          </Grid>
+          <img src="/images/Bratwurst.png" />
+          <Button
+            className={classes.bidMore}
+            wrapper="big"
+            variant="bigInner"
+            text="BID MORE"
+            onClick={this.handleCloseClick}
+          />
+        </div>
+        <span className={classes.banner} />
+        <div className={classes.space} />
+        <div className={classes.circle}>
+          <Typography className={classes.name}>BRATWURST</Typography>
+          <img src="/images/sausage-pill.svg" />
+          <span className={classes.pillCopy}>Rare</span>
+        </div>
+        <Typography className={classes.description}>
+          Bratwurst is a type of German sausage made from veal, beef, or most
+          commonly pork. The name is derived from the Old High German Brätwurst,
+          from brät-, finely chopped meat, and Wurst, sausage, although in
+          modern German it is often associated with the verb braten, to pan fry
+          or roast.
+        </Typography>
+        <img src="/images/big-sausage.svg" />
+        <Grid container justify="center" className={classes.bids}>
+          <Grid item xs={6}>
+            <Typography className={classes.text}>Leading bid</Typography>
+            <div className={classes.divider} />
+            <Typography className={classes.number}>
+              45 <span>eth</span>
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography className={classes.text}>Ends in</Typography>
+            <div className={classes.divider} />
+            <Typography className={classnames(classes.number, classes.seconds)}>
+              63 <span>sec</span>
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          justify="center"
+          spacing={16}
+          className={classes.bidButtons}
+          id="bid"
+        >
+          <Grid item>
+            <label className={classes.label}>
+              <input
+                name="bid"
+                type="number"
+                placeholder="0"
+                onChange={this.onBidChange}
+              />
+              <span>ETH</span>
+            </label>
+          </Grid>
+          <Grid item>
+            <Button text="BID" onClick={this.handleOpenClick} />
+          </Grid>
+        </Grid>
+        <img src="/images/divider-big.svg" />
+        <Grid className={classes.history}>
+          <Typography className={classes.title}>BID HISTORY</Typography>
+          <div className={classes.count}>110 bids</div>
+          <Grid className={classes.bidWrapper}>
+            {[1, 2, 3, 4, 5].map((el, index) => (
+              <RowItem key={index} />
+            ))}
+          </Grid>
+        </Grid>
       </Grid>
-    </Grid>
-    <span className={classes.ornament} />
-    <Social className={classes.social} />
-  </Grid>
-)
+    )
+  }
+}
 
 export default withStyles(styles)(Layout)
