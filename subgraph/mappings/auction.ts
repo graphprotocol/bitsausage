1fc63e04bf238e2d3b4469c88520cc8177b1e968
Nena <nena@thegraph.com>
2018-09-09 10:44:55 +0200

    Subgraph: Create

---
+++ subgraph/mappings/auction.ts
@@ -0,0 +1,3 @@
+export function applicationWhitelisted(event: LogNewBid): void {}
+export function applicationWhitelisted(event: LogAuctionOpen): void {}
+export function applicationWhitelisted(event: LogAuctionClosed): void {}
